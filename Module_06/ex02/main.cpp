8b447d57c0d38dff3eb6d2243b63685ffa8cb147
Dot99 <gui.rocha.felix@gmail.com>
2025-01-08 09:33:57 +0000

    Prog Module_06/ex02

---
+++ Module_06/ex02/main.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gude-jes <gude-jes@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 09:26:42 by gude-jes          #+#    #+#             */
+/*   Updated: 2025/01/08 09:33:28 by gude-jes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+Base *generate(void)
+{
+	srand(time(NULL));
+	int i = rand() % 3;
+
+	if (i == 0)
+		return new A();
+	else if (i == 1)
+		return new B();
+	else
+		return new C();
+}
+
+void identify_from_pointer(Base *p)
+{
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify_from_reference(Base &p)
+{
+	try
+	{
+		A &a = dynamic_cast<A &>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		B &b = dynamic_cast<B &>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+	try
+	{
+		C &c = dynamic_cast<C &>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	}
+	catch (std::bad_cast &bc)
+	{
+	}
+}
+
+int main()
+{
+	Base *base = generate();
+	identify_from_pointer(base);
+	identify_from_reference(*base);
+	delete base;
+	return 0;
+}
