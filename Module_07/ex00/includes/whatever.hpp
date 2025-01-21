3878818dab3373576c065ed1d730e2b8a82b7c42
Dot99 <gui.rocha.felix@gmail.com>
2025-01-21 16:00:35 +0000

    fix Module_07

--- Module_07/ex00/includes/whatever.hpp
+++ Module_07/ex00/includes/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: gude-jes <gude-jes@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 10:38:50 by gude-jes          #+#    #+#             */
-/*   Updated: 2025/01/09 11:16:35 by gude-jes         ###   ########.fr       */
+/*   Updated: 2025/01/21 15:59:43 by gude-jes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ void swap(T &a,T &b) {
 
 template <typename U>
 
-const U &min(const U a,const U b) {
+U min(const U a,const U b) {
 	return (a < b ? a : b);
 }
 
 template <typename V>
 
-const V &max(const V a,const V b) {
+V max(const V a,const V b) {
 	return (a > b ? a : b);
 }
